6ce26810acbed6c1980bc4a66bd1521924ffb18c
rohitdasu <dasurohit123@gmail.com>
2022-08-23 23:35:22 +0530

    feat: added landing page

--- FE/pages/index.tsx
+++ FE/pages/index.tsx
@@ -2,83 +2,100 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import tw from 'twin.macro';
+import Lottie from 'lottie-react-web';
+import animation from '../public/animation-lottie.json';
 
 const Home: NextPage = () => {
   return (
-    <div tw="flex min-h-screen flex-col items-center justify-center py-2">
+    <div tw="flex min-h-screen flex-col items-center justify-center">
       <Head>
-        <title>Title</title>
+        <title>projectmate</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main tw="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 tw="text-6xl font-bold">
-          Welcome to{' '}
-          <a tw="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p tw="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code tw="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div tw="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+      <nav tw="flex h-24 w-full items-center bg-white justify-between border-t px-20 shadow-md">
+        <a href="/">
+          <span tw="text-2xl font-semibold font-mono text-gray-900">
+            projectmate
+          </span>
+        </a>
+        <div tw="flex items-center justify-between w-[100px]">
           <a
-            href="https://nextjs.org/docs"
-            tw="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            href="https://github.com/rohitdasu/projectmate"
+            target="_blank"
+            rel="noreferrer"
           >
-            <h3 tw="text-2xl font-bold">Documentation &rarr;</h3>
-            <p tw="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
+            <Image src="/github.svg" alt="Vercel Logo" width={40} height={40} />
           </a>
-
           <a
-            href="https://nextjs.org/learn"
-            tw="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            href="https://discord.gg/M2BMPdku"
+            target="_blank"
+            rel="noreferrer"
           >
-            <h3 tw="text-2xl font-bold">Learn &rarr;</h3>
-            <p tw="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            tw="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 tw="text-2xl font-bold">Examples &rarr;</h3>
-            <p tw="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            tw="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 tw="text-2xl font-bold">Deploy &rarr;</h3>
-            <p tw="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <Image
+              tw="ml-4"
+              src="/discord.svg"
+              alt="Vercel Logo"
+              width={40}
+              height={40}
+            />
           </a>
         </div>
-      </main>
+      </nav>
 
-      <footer tw="flex h-24 w-full items-center justify-center border-t">
-        <a
-          tw="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+      <main tw="flex w-full flex-1 text-center">
+        <div tw="flex flex-col items-center justify-center w-1/2 px-20 border-r">
+          <h1 tw="text-4xl font-bold font-bold text-gray-900">
+            Welcome to projectmate - a place where you find project mates
+          </h1>
+
+          <p tw="mt-6 text-2xl font-normal text-gray-800">
+            Find contributors and find projects to contribute
+          </p>
+
+          <button
+            type="button"
+            tw="mt-16 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-8 py-3 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+          >
+            JOIN US
+          </button>
+        </div>
+        <div>
+          <Lottie
+            tw="h-[200px] w-[100px]"
+            options={{
+              animationData: animation,
+            }}
+          />
+        </div>
+      </main>
+      <footer tw="flex flex-col h-24 w-full items-center bg-white justify-center border-t px-20 shadow-md">
+        <p tw="text-lg font-light text-gray-800 mb-2 pb-0">
+          Shout-out to our contributors
+        </p>
+        <div tw="flex items-center justify-evenly w-1/5">
+          <Image
+            src="https://avatars.githubusercontent.com/u/66621672?s=64&v=4"
+            height={30}
+            width={30}
+            tw="rounded-full bg-black"
+            alt=""
+          />
+          <Image
+            src="https://avatars.githubusercontent.com/u/48400770?s=64&v=4"
+            height={30}
+            width={30}
+            tw="rounded-full bg-black"
+            alt=""
+          />
+          <Image
+            src="https://avatars.githubusercontent.com/u/107752425?s=88&u=4100b00ef035da1c3a7337800bf9957714b9a912&v=4"
+            height={30}
+            width={30}
+            tw="rounded-full bg-black"
+            alt=""
+          />
+        </div>
       </footer>
     </div>
   );
