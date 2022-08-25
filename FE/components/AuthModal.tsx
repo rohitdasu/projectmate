24d3f03baa8714b5261a999b58c1c8f1ef0a7b28
rohitdasu <dasurohit123@gmail.com>
2022-08-25 20:18:05 +0530

    fix: improve fe code

--- FE/components/AuthModal.tsx
+++ FE/components/AuthModal.tsx
@@ -1,6 +1,7 @@
 import { Dialog, Transition } from '@headlessui/react';
-import { Fragment, useState } from 'react';
+import { Fragment } from 'react';
 import Image from 'next/image';
+import tw from 'twin.macro';
 function AuthModal({ isOpen, setIsOpen }: any) {
   function closeModal() {
     setIsOpen(false);
@@ -9,7 +10,7 @@ function AuthModal({ isOpen, setIsOpen }: any) {
   return (
     <>
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Dialog as="div" tw="relative z-10" onClose={closeModal}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -19,11 +20,11 @@ function AuthModal({ isOpen, setIsOpen }: any) {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
+            <div tw="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
 
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full  items-center justify-center p-4 text-center">
+          <div tw="fixed inset-0 overflow-y-auto">
+            <div tw="flex min-h-full items-center justify-center p-4 text-center">
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
@@ -33,15 +34,15 @@ function AuthModal({ isOpen, setIsOpen }: any) {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel tw="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg text-center font-medium leading-6 text-gray-900"
+                    tw="text-lg text-center font-medium leading-6 text-gray-900"
                   >
                     Continue with your social accounts.
                   </Dialog.Title>
-                  <div className=" mx-4  flex h-[100px] justify-evenly  items-center ">
-                    <button className="items-center flex space-x-2 ">
+                  <div tw="mx-4  flex h-[100px] justify-evenly  items-center ">
+                    <button tw="items-center flex space-x-2 ">
                       <Image
                         src="/github.svg"
                         height={30}
@@ -51,7 +52,7 @@ function AuthModal({ isOpen, setIsOpen }: any) {
                       <span>Github</span>
                     </button>
 
-                    <button className="items-center flex space-x-2">
+                    <button tw="items-center flex space-x-2">
                       <Image
                         src="/google.png"
                         alt="google-logo"
