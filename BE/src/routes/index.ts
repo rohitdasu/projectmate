8191b0a164f649357109fd378ef17d36897809d2
rohitdasu <dasurohit123@gmail.com>
2022-08-27 14:29:48 +0530

    feat: added get projects request

--- BE/src/routes/index.ts
+++ BE/src/routes/index.ts
@@ -1,15 +1,19 @@
 import express, { Router, Request, Response, NextFunction } from "express";
-import { HomeSchema } from "../database/models/Home"
+import { HomeSchema } from "../database/models/Home";
+import { PostSchema } from "../database/models/Post";
 
 const router: Router = express();
 
 const main = () => {
-
   router.get("/", async (req: Request, res: Response) => {
-      const data = await HomeSchema.find()
-      res.send(data[0]);
+    const data = await HomeSchema.find();
+    res.send(data[0]);
   });
 
+  router.get("/projects", async (req: Request, res: Response) => {
+    const data = await PostSchema.find();
+    res.send(data);
+  });
 };
 
 main();
