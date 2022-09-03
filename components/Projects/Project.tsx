973a65446e69ecbbbec37bca55025fe9908a894c
rohitdasu <dasurohit123@gmail.com>
2022-09-03 15:02:22 +0530

    fix: smaller screen responsive

--- components/Projects/Project.tsx
+++ components/Projects/Project.tsx
@@ -10,7 +10,7 @@ export const Project = ({ title, description, tags }: any) => {
         Mode ? 'text-white bg-box-color border-gray-200' : 'text-dark-color'
       } text-white shadow-md w-full px-2 rounded-md items-center md:flex justify-center space-y-2`}
     >
-      <div className="w-[250px] md:w-[500px] mx-auto h-auto">
+      <div className="w-[250px] md:w-[500px] mx-auto h-auto p-4">
         <Image
           src={'/open-source.png'}
           alt={title}
@@ -19,15 +19,15 @@ export const Project = ({ title, description, tags }: any) => {
           className="object-contain  "
         />
       </div>
-      <div className="flex flex-col space-y-2">
+      <div className="flex flex-col space-y-2 p-2">
         <h1 className="font-semibold text-xl">{title}</h1>
         <p className="text-[14px] ">{description}</p>
-        <div className="flex justify-between items-center ">
-          <div className="flex  space-x-2">
+        <div className="flex flex-col md:flex-row md:justify-between md:items-center">
+          <div className="flex space-x-2">
             {tags.map((tag: any, i: any) => (
               <p
                 key={i}
-                className={`p-2  ${
+                className={`p-2 ${
                   !Mode && 'text-white'
                 } text-[14px] bg-primary-color cursor-pointer rounded-md`}
               >
@@ -36,7 +36,7 @@ export const Project = ({ title, description, tags }: any) => {
             ))}
           </div>
           <button
-            className={`p-2 sm:my-0 my-2 flex items-center space-x-2 bg-secondary-color ${
+            className={`p-2 mt-2 sm:my-0 flex items-center space-x-2 bg-secondary-color ${
               !Mode && 'text-white'
             } rounded-md`}
           >
