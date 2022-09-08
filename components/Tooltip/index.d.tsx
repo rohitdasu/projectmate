1f74aefe5ef6439e6d83b2fcffd827c4af724060
Vlad Brok <vlad.brok99@gmail.com>
2022-09-08 19:22:18 +0300

    Fix recursive type reference

--- components/Tooltip/index.d.tsx
+++ components/Tooltip/index.d.tsx
@@ -1,8 +1,7 @@
 import { ReactNode } from 'react';
-import { TooltipProps as Props } from 'react-tooltip-lite';
 
 declare module 'react-tooltip-lite' {
-  export interface TooltipProps extends Props {
+  export interface TooltipProps {
     children: ReactNode;
   }
 }
