f99132f3a2fd0fe07d1a9e8a60848247f73c9c74
leewan-09 <leewandowski98@gmail.com>
2022-09-12 15:39:43 +0500

    switch case

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -7,62 +7,61 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // Handle post request
-  if (req.method === 'POST') {
-    const { email, firebaseUID } = req.body;
-    try {
-      const data = await addUser({ email, firebaseUID });
-      return successResponse({
-        res,
-        message: 'user created successfully',
-        results: data,
-        success: true,
-        statusCode: 201,
-      });
-    } catch (error) {
-      if (error.code === 'P2002') {
+  switch (req.method) {
+    case 'GET':
+      try {
+        const data = await getUsers();
+        return successResponse({
+          res,
+          message: '',
+          results: data,
+          statusCode: 200,
+          success: true,
+        });
+      } catch (error) {
         return errorResponse({
           res,
-          message: 'User already exist',
-          statusCode: 400,
+          message: 'Internal Error',
+          statusCode: 500,
           success: false,
         });
       }
+    case 'POST':
+      const { email, firebaseUID } = req.body;
+      try {
+        const data = await addUser({ email, firebaseUID });
+        return successResponse({
+          res,
+          message: 'user created successfully',
+          results: data,
+          success: true,
+          statusCode: 201,
+        });
+      } catch (error) {
+        if (error.code === 'P2002') {
+          return errorResponse({
+            res,
+            message: 'User already exist',
+            statusCode: 400,
+            success: false,
+          });
+        }
+        return errorResponse({
+          res,
+          message: 'Internal Error',
+          statusCode: 500,
+          success: false,
+        });
+      }
+
+    default:
       return errorResponse({
         res,
-        message: 'Internal Error',
-        statusCode: 500,
+        message: 'Bad Request',
+        statusCode: 400,
         success: false,
       });
-    }
   }
-
-  if (req.method === 'GET') {
-    try {
-      const data = await getUsers();
-      return successResponse({
-        res,
-        message: '',
-        results: data,
-        statusCode: 200,
-        success: true,
-      });
-    } catch (error) {
-      return errorResponse({
-        res,
-        message: 'Internal Error',
-        statusCode: 500,
-        success: false,
-      });
-    }
-  }
-
-  return errorResponse({
-    res,
-    message: 'Bad Request',
-    statusCode: 400,
-    success: false,
-  });
 }
 
 async function addUser(args: { email: string; firebaseUID: string }) {
