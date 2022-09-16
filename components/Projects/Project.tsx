692099a01c1be3670f4134a3b50d9a88d6db9b6e
Rohit <48400770+rohitdasu@users.noreply.github.com>
2022-09-16 22:21:52 +0530

    fix: improved project card and buttons (#139)

--- components/Projects/Project.tsx
+++ components/Projects/Project.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { useAppSelector } from '../../app/hooks';
 import { useDispatch } from 'react-redux';
@@ -12,6 +12,10 @@ export const Project = ({ title, description, tags, author }: ProjectProps) => {
   const handleContribute = () => {
     return;
   };
+  const [innerWidth, setInnerWidth] = useState(0);
+  useEffect(() => {
+    setInnerWidth(window.innerWidth);
+  }, []);
   return (
     <div className="w-full p-2 md:p-0">
       <div
@@ -19,28 +23,27 @@ export const Project = ({ title, description, tags, author }: ProjectProps) => {
           mode
             ? 'text-white bg-box-color border border-[#363D45]'
             : 'text-dark-color border border-[#C9C9C9]'
-        } text-white shadow-md rounded-md items-center md:flex justify-center space-y-2`}
+        } text-white shadow-md rounded-md items-center flex-col md:flex md:flex-row justify-center space-y-2`}
       >
-        <div className="w-[250px] md:w-[500px] mx-auto h-auto p-4">
+        <div className="w-full p-2 flex justify-center items-center">
           <Image
             src={'/open-source.png'}
             alt={title}
-            height={'300'}
-            width={'300'}
-            className="object-contain  "
+            height={800}
+            width={innerWidth/1.2}
           />
         </div>
-        <div className="flex flex-col space-y-2 p-2">
+        <div className="h-full flex flex-col justify-between space-y-2 p-2">
           <h1 className="font-semibold text-xl">
             {title} - <span className="text-lg font-light">{author}</span>
           </h1>
-          <p className="text-[14px] ">{description}</p>
+          <p className="text-sm ">{description}</p>
           <div className="flex flex-col md:flex-row md:justify-between md:items-center">
             <div className="flex space-x-2 pb-2 md:pb-0">
               {tags.map((tag: string, i: number) => (
                 <p
                   key={i}
-                  className={`px-2 py-1 text-[14px] rounded-full cursor-pointer ${
+                  className={`px-2 py-1 text-sm rounded-full cursor-pointer ${
                     !mode
                       ? 'text-[#373737] bg-[#dedede] border border-[#c9c9c9]'
                       : 'border border-[#363d45] bg-[#31363e]'
@@ -54,7 +57,7 @@ export const Project = ({ title, description, tags, author }: ProjectProps) => {
               onClick={() =>
                 userLogged ? handleContribute : dispatch(openModal())
               }
-              className={`p-2 mt-2 sm:my-0 flex justify-center items-center space-x-2 bg-secondary-color ${
+              className={`px-2 py-1.5 mt-2 sm:my-0 flex border border-[#999] focus:ring focus:bg-blue-800 bg-secondary-color ${
                 !mode && 'text-white'
               } rounded-md`}
             >
