c3c03375fce61966d3b5148990289af9a2e27cbe
Rounak-28 <95576871+Rounak-28@users.noreply.github.com>
2022-09-18 20:20:25 +0530

    fixed login page

--- components/AuthModal/AuthModal.tsx
+++ components/AuthModal/AuthModal.tsx
@@ -112,9 +112,7 @@ export const AuthModal = ({ title }: IAuthData) => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel
-                  className={`w-full h-[450px] bg-background-1 max-w-md transform overflow-hidden rounded-2xl  p-6 text-left align-middle shadow-xl transition-all`}
-                >
+                <Dialog.Panel className="w-full h-[450px] bg-background-1 max-w-md transform overflow-hidden rounded-2xl  p-6 text-left align-middle shadow-xl transition-all border-2 border-[#FD973C]">
                   <span
                     className={`text-2xl flex w-full justify-center items-center  md:space-x-2 font-bold font-mono text-gray-900 uppercase text-foreground-1 mb-12`}
                   >
@@ -124,14 +122,14 @@ export const AuthModal = ({ title }: IAuthData) => {
                   </span>
                   <Dialog.Title
                     as="h3"
-                    className={`text-base lg:text-lg text-center font-medium leading-6 mb-20 text-gray-900 text-foreground-1`}
+                    className="text-base lg:text-lg text-center font-medium leading-6 mb-20 text-gray-900 text-foreground-1"
                   >
                     {title}
                   </Dialog.Title>
                   <div tw="mx-4 w-[75%] flex mx-auto h-[100px] justify-evenly space-y-8 flex-col items-center">
                     <button
                       tw="items-center flex space-x-2"
-                      className={`w-full border-2 py-2 px-3 rounded-lg border-color-1`}
+                      className="w-full border-2 py-2 px-3 rounded-lg border-color-1"
                       onClick={() => handleSignin(ProviderType.GITHUB)}
                     >
                       <svg
@@ -148,16 +146,14 @@ export const AuthModal = ({ title }: IAuthData) => {
                           d="M12 0C5.37 0 0 5.37 0 12C0 17.31 3.435 21.795 8.205 23.385C8.805 23.49 9.03 23.13 9.03 22.815C9.03 22.53 9.015 21.585 9.015 20.58C6 21.135 5.22 19.845 4.98 19.17C4.845 18.825 4.26 17.76 3.75 17.475C3.33 17.25 2.73 16.695 3.735 16.68C4.68 16.665 5.355 17.55 5.58 17.91C6.66 19.725 8.385 19.215 9.075 18.9C9.18 18.12 9.495 17.595 9.84 17.295C7.17 16.995 4.38 15.96 4.38 11.37C4.38 10.065 4.845 8.985 5.61 8.145C5.49 7.845 5.07 6.615 5.73 4.965C5.73 4.965 6.735 4.65 9.03 6.195C9.99 5.925 11.01 5.79 12.03 5.79C13.05 5.79 14.07 5.925 15.03 6.195C17.325 4.635 18.33 4.965 18.33 4.965C18.99 6.615 18.57 7.845 18.45 8.145C19.215 8.985 19.68 10.05 19.68 11.37C19.68 15.975 16.875 16.995 14.205 17.295C14.64 17.67 15.015 18.39 15.015 19.515C15.015 21.12 15 22.41 15 22.815C15 23.13 15.225 23.505 15.825 23.385C18.2072 22.5807 20.2772 21.0497 21.7437 19.0074C23.2101 16.965 23.9993 14.5143 24 12C24 5.37 18.63 0 12 0Z"
                         />
                       </svg>
-                      <span
-                        className={`flex justify-center w-full text-base lg:text-lg text-foreground-1`}
-                      >
+                      <span className="flex justify-center w-full text-base lg:text-lg text-foreground-1">
                         Continue with Github
                       </span>
                     </button>
 
                     <button
                       tw="items-center flex space-x-2"
-                      className={`w-full border-2 py-2 px-3 rounded-lg border-color-1`}
+                      className="w-full border-2 py-2 px-3 rounded-lg border-color-1"
                       onClick={() => handleSignin(ProviderType.GOOGLE)}
                     >
                       <svg
@@ -183,9 +179,7 @@ export const AuthModal = ({ title }: IAuthData) => {
                           d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
                         />
                       </svg>
-                      <span
-                        className={`flex justify-center w-full text-base lg:text-lg text-foreground-1`}
-                      >
+                      <span className="flex justify-center w-full text-base lg:text-lg text-foreground-1">
                         Continue with Google
                       </span>
                     </button>
