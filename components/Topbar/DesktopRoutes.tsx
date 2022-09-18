67c686bd866b1b8abe042cce9ad021c09eb5032a
Yazdun <83041367+Yazdun@users.noreply.github.com>
2022-09-18 09:13:46 +0430

    fix: navbar active route display (#152)
    * fix: navbar active state
    
    * styles: added more poppins font variations
    
    * fix: navbar route active state

--- components/Topbar/DesktopRoutes.tsx
+++ components/Topbar/DesktopRoutes.tsx
@@ -1,15 +1,25 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { appRoutes } from './data';
 
 export const DesktopRoutes = () => {
+  const { pathname } = useRouter();
+
   return (
     <ul className="hidden gap-5 capitalize md:flex">
       {appRoutes.map((route) => {
         const { title, url, Icon, anchorTagProps } = route;
+        const isCurrent = pathname === url || pathname.includes(title);
+
         return (
           <li key={title}>
             <Link href={url}>
-              <a className="flex items-center gap-1" {...anchorTagProps}>
+              <a
+                className={`flex items-center gap-1 ${
+                  isCurrent && 'text-primary-1'
+                }`}
+                {...anchorTagProps}
+              >
                 <Icon />
                 {title}
               </a>
