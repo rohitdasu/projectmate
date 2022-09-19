09f40db09ef121f71293bf9c78eb527934f24a4e
leewan-09 <leewandowski98@gmail.com>
2022-09-20 01:57:24 +0500

    base auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,35 @@
+import NextAuth, { type NextAuthOptions } from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+import GithubProvider from 'next-auth/providers/github';
+// import GoogleProvider from 'next-auth/providers/google';
+
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+  throw Error('MISSING GITHUB CLIENT ID OR SECRET KEY');
+}
+
+// const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+// const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+// if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
+//   throw Error('MISSING GOOGLE CLIENT ID OR SECRET KEY');
+// }
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+    }),
+    // GoogleProvider({
+    //   clientId: GOOGLE_CLIENT_ID,
+    //   clientSecret: GOOGLE_CLIENT_SECRET,
+    // }),
+  ],
+};
+
+export default NextAuth(authOptions);
