d73dfc8f8f14e6068b358b8370e09dba9e670c86
Yazdun <yazduntube@gmail.com>
2022-09-20 10:52:22 +0430

    cleaned up pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,39 +1,30 @@
 import { SharedLayout } from '@/components/Layouts/SharedLayout';
 import { NextPage } from 'next';
-import React, { useEffect, useState } from 'react';
 import { Toaster } from 'react-hot-toast';
+import useSWR from 'swr';
 import { ContributorList } from '../components';
 
 const About: NextPage = () => {
-  const [contributors, setContributors] = useState([]);
+  const fetcher = (...args: Parameters<typeof fetch>) =>
+    fetch(...args).then((res) => res.json());
 
-  useEffect(() => {
-    fetch(
-      'https://api.github.com/repos/rohitdasu/projectmate/contributors'
-    ).then((response) => {
-      const json = response.json();
-      json
-        .then((data) => {
-          setContributors(data);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    });
-  }, []);
+  const { data } = useSWR(
+    'https://api.github.com/repos/rohitdasu/projectmate/contributors',
+    fetcher
+  );
 
   return (
     <SharedLayout title="About">
       <main tw="flex mt-[8rem]  lg:w-full flex-1 ">
         <Toaster />
       </main>
-      <footer className="flex flex-col items-center justify-center w-full h-24 px-6 mt-auto shadow-md lg:px-20 bg-background-1">
+      <footer className="flex flex-col items-center justify-center w-full h-24 px-6 mt-auto lg:px-20 bg-background-1">
         <p className="pb-0 mb-2 text-lg font-light">
           Shout-out to our contributors
         </p>
 
         <div tw="flex items-center justify-around w-[80%] lg:w-1/5">
-          <ContributorList contributors={contributors} />
+          <ContributorList contributors={data} />
         </div>
       </footer>
     </SharedLayout>
