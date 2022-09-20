5b05772d35f7bf2e7b1f07f83490137a001f5542
JakubChorzepa <jakubchorzepa@gmail.com>
2022-09-20 14:41:18 +0200

    Delete loading state in ProjectList, delete .vscode directory

--- components/Projects/ProjectsList.tsx
+++ components/Projects/ProjectsList.tsx
@@ -7,22 +7,15 @@ export const ProjectsList = () => {
   const fetcher = (...args: Parameters<typeof fetch>) =>
     fetch(...args).then((res) => res.json());
   const { data, error } = useSWR('/api/project', fetcher);
-  const [isLoading, setIsLoading] = useState(true);
 
   // Generate array of specified length with random key value
   const skeletonProjectsToLoad = Array.from({length: 10}, () => (Math.random() + 1).toString(36).substring(7));
-
-  useEffect(() => {
-    if(data) {
-      setIsLoading(false)
-    }
-  }, [data])
 
   if (error) return <div className="m-auto text-lg my-5">Failed to load projects</div>;
 
   return (
     <div className="container gap-5 p-3 m-auto md:p-5 md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {isLoading ? (
+      {!data ? (
         skeletonProjectsToLoad.map((randomKey) => <ProjectSkeleton key={randomKey} />)
         ): (
           <>
