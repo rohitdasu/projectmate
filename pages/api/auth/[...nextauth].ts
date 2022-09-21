9669f0fa272e260677dfc651c40c45b134e48872
leewan-09 <leewandowski98@gmail.com>
2022-09-21 13:48:48 +0500

    improvements

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,31 +7,18 @@ import { prisma } from '@/lib/prisma';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
-const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
-if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
-  throw Error('MISSING GITHUB CLIENT ID OR SECRET KEY');
-}
-
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-
-if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
-  throw Error('MISSING GOOGLE CLIENT ID OR SECRET KEY');
-}
-
 export const ProductionAuthOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
 
   providers: [
     GithubProvider({
-      clientId: GITHUB_CLIENT_ID,
-      clientSecret: GITHUB_CLIENT_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID || '',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
     }),
 
     GoogleProvider({
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
   ],
 };
