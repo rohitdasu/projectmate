8ce9a5a87d9966da7e1971f27ffde5ae8939096e
leewan-09 <leewandowski98@gmail.com>
2022-09-21 13:59:06 +0500

    .env template updated

--- lib/getServerAuthSession.ts
+++ lib/getServerAuthSession.ts
@@ -1,5 +1,3 @@
-// Wrapper for unstable_getServerSession https://next-auth.js.org/configuration/nextjs
-
 import type { GetServerSidePropsContext } from 'next';
 import { unstable_getServerSession } from 'next-auth';
 import {
