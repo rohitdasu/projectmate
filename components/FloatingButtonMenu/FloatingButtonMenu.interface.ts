3bf3bc2356a77ba1d8626fcf09b26adab06e994b
Jakub <74047866+JakubChorzepa@users.noreply.github.com>
2022-09-23 17:20:57 +0200

    chore: replace old floating button menu (#172)
    * Add line clamp plugin, fixed ProjectSkeleton height
    
    * Revert changes from previus PR at AuthModal title
    
    * Add line clamp plugin, fixed ProjectSkeleton height
    
    * Add tailwind theme dark class, change ProjectSkeleton look for dark theme
    
    * Delete loading state in ProjectList, delete .vscode directory
    
    * Create basic working floating button menu
    
    * Finish first version of floating button menu
    
    * Remove react-floating-button-menu package
    
    * Fit prettier lint errors
    
    * Export FloatingMenuProps in index
    
    * Change interface export

---
+++ components/FloatingButtonMenu/FloatingButtonMenu.interface.ts
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+
+export type FloatingMenuProps = {
+  toggleMenu: () => void;
+  isOpen: boolean;
+  slideSpeed: number;
+  bgColor: string;
+  spacing: number;
+  mainButton: ReactElement;
+  children: ReactElement | ReactElement[];
+};
