f98e3b6b86e5d7f5fcbb21ae87dffc6e5e88ab35
JakubChorzepa <jakubchorzepa@gmail.com>
2022-09-23 16:31:39 +0200

    Fit prettier lint errors

--- components/FloatingButtonMenu/FloatingButtonMenu.interface.ts
+++ components/FloatingButtonMenu/FloatingButtonMenu.interface.ts
@@ -1,11 +1,11 @@
-import { ReactElement } from "react"
+import { ReactElement } from 'react';
 
 export type FloatingMenuProps = {
-  toggleMenu: () => void,
-  isOpen: boolean,
-  slideSpeed: number,
-  bgColor: string,
-  spacing: number,
-  mainButton: ReactElement,
-  children: ReactElement | ReactElement[]
-}
+  toggleMenu: () => void;
+  isOpen: boolean;
+  slideSpeed: number;
+  bgColor: string;
+  spacing: number;
+  mainButton: ReactElement;
+  children: ReactElement | ReactElement[];
+};
--- components/FloatingButtonMenu/FloatingMenu.tsx
+++ components/FloatingButtonMenu/FloatingMenu.tsx
@@ -1,31 +1,42 @@
-import { FloatingMenuProps } from "./FloatingButtonMenu.interface";
+import { FloatingMenuProps } from './FloatingButtonMenu.interface';
 import { Children } from 'react';
 
-export const FloatingMenu = ({ 
-  toggleMenu, isOpen, slideSpeed, mainButton, bgColor, spacing, children 
+export const FloatingMenu = ({
+  toggleMenu,
+  isOpen,
+  slideSpeed,
+  mainButton,
+  bgColor,
+  spacing,
+  children,
 }: FloatingMenuProps) => {
   const arrayChildren = Children.toArray(children);
 
   return (
-    <ul
-      className={`flex flex-col-reverse m-0 p-0`}
-    >
+    <ul className={`flex flex-col-reverse m-0 p-0`}>
       <li
         onClick={toggleMenu}
-        style={{backgroundColor: bgColor, marginTop: `${spacing}px`}}
-        className={`rounded-full relative grid place-items-center lg:cursor-pointer p-3`}>
-          <div className={`transition ease-in-out ${isOpen ? 'rotate-0' : 'rotate-[360deg]'} duration-${slideSpeed}`}>
-            {mainButton}
-          </div>
+        style={{ backgroundColor: bgColor, marginTop: `${spacing}px` }}
+        className={`rounded-full relative grid place-items-center lg:cursor-pointer p-3`}
+      >
+        <div
+          className={`transition ease-in-out 
+          ${isOpen ? 'rotate-0' : 'rotate-[360deg]'} duration-${slideSpeed}`}
+        >
+          {mainButton}
+        </div>
       </li>
       {arrayChildren.map((child, index) => (
         <li
           key={index}
-          style={{backgroundColor: bgColor, marginTop: `${spacing}px`}}
-          className={`translate-y-full z-0 rounded-full relative grid place-items-center lg:cursor-pointer p-3 transition ease-in-out ${isOpen ? 'translate-y-0 scale-100' : 'scale-0'} duration-${slideSpeed}`}>
-            {child}
+          style={{ backgroundColor: bgColor, marginTop: `${spacing}px` }}
+          className={`translate-y-full z-0 rounded-full relative grid place-items-center lg:cursor-pointer p-3 transition ease-in-out
+          ${isOpen ? 'translate-y-0 scale-100' : 'scale-0'}
+          duration-${slideSpeed}`}
+        >
+          {child}
         </li>
       ))}
     </ul>
-  )
-}
+  );
+};
