7b8aac6d8de6624d4c60ff81b5de2ea95889bc15
hs05june <harpreet.singh.cse21@itbhu.ac.in>
2022-10-02 21:40:32 +0530

    UI improvement of About Page

--- components/ContributorList/ContributorList.tsx
+++ components/ContributorList/ContributorList.tsx
@@ -8,22 +8,52 @@ export const ContributorList = ({ contributors }: ContributorProps) => {
       {contributors &&
         contributors.length > 0 &&
         contributors.map((contributor, index) => (
-          <a
+          <div
+            className="our-team relative w-60 overflow-hidden text-center"
             key={index}
-            href={contributor.html_url}
-            target="_blank"
-            rel="noreferrer"
-            className="mx-2"
-            title={contributor.html_url.split('/')[3]}
           >
             <Image
               src={contributor.avatar_url}
               alt={contributor.html_url.split('/')[3]}
-              width={30}
-              height={30}
-              className="rounded-full bg-black"
+              width={200}
+              height={200}
+              className="object-cover"
             />
-          </a>
+            <div className="team-content">
+              <h3 className="title font-700 text-lg">
+                {contributor.html_url.split('/')[3]}
+              </h3>
+              <ul className="icon">
+                <li className="inline-block">
+                  <a
+                    target="_blank"
+                    aria-label="github"
+                    href={contributor.html_url}
+                    className="
+              iconify
+              block h-10
+              w-10
+              rounded-full
+              border
+              border-solid
+              text-center
+              text-base
+              leading-9
+              hover:border-gray-900 hover:bg-gray-900
+            "
+                    rel="noreferrer"
+                  >
+                    <span
+                      className="iconify m-auto mt-1 text-xl"
+                      data-icon="icon"
+                    >
+                      <Image src="icon.svg" />
+                    </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         ))}
     </Fragment>
   );
--- pages/about.tsx
+++ pages/about.tsx
@@ -15,7 +15,7 @@ const About: NextPage = () => {
   return (
     <SharedLayout title="About">
       <div className="flex-1 lg:w-full "></div>
-      <footer className="mt-auto flex h-24 w-full flex-col items-center justify-center bg-background-1 px-6 lg:px-20">
+      <footer className="mt-auto flex h-full w-full flex-col items-center justify-center bg-background-1 px-6 lg:px-20">
         <p className="mb-2 pb-0 text-lg font-light">
           Shout-out to our contributors
         </p>
