eb9f9a8793b0e558bcb88d63737f95bfc78f4a32
Yazdun <yazduntube@gmail.com>
2022-10-19 12:52:38 +0330

    added ts interfaces to the form elements

--- components/FormElements/Error.tsx
+++ components/FormElements/Error.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { framer_error } from './framer';
 
-export const InputError = ({ message }) => {
+interface IProps {
+  message: string;
+}
+
+export const InputError: React.FC<IProps> = ({ message }) => {
   return (
     <motion.p
       className="rounded-md bg-red-600 px-1 text-white dark:bg-red-700"
