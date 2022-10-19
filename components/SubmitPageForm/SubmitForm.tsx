3341fd618eac1482f7aef5b7ccee7994fec99c4c
Yazdun <yazduntube@gmail.com>
2022-10-19 14:47:04 +0330

    made progress on creating submit project form

--- components/SubmitPageForm/SubmitForm.tsx
+++ components/SubmitPageForm/SubmitForm.tsx
@@ -1,15 +1,28 @@
 import { FormProvider, useForm } from 'react-hook-form';
 import { Input } from '../FormElements';
-import { project_name_validation } from '../../utils/input_validators';
+import {
+  project_name_validation,
+  project_url_validation,
+  project_desc_validation,
+  project_content_validation,
+} from '../../utils/input_validators';
 
 export const SubmitForm = () => {
   const methods = useForm();
 
   return (
     <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit((data) => console.log(data))}>
+      <form
+        onSubmit={methods.handleSubmit((data) => console.log(data))}
+        className="flex flex-col items-start gap-3"
+      >
         <Input {...project_name_validation} />
-        <button>submit</button>
+        <Input {...project_url_validation} />
+        <Input {...project_desc_validation} />
+        <Input {...project_content_validation} />
+        <button className="rounded-md bg-blue-600 p-3 text-white">
+          Submit Project
+        </button>
       </form>
     </FormProvider>
   );
--- utils/input_validators.ts
+++ utils/input_validators.ts
@@ -7,7 +7,54 @@ export const project_name_validation = {
   validation: {
     required: {
       value: true,
-      message: 'project name is required      ',
+      message: 'project name is required',
     },
   },
+};
+
+export const project_url_validation = {
+  name: 'repo',
+  label: 'Repository URL',
+  type: 'text',
+  id: 'repo',
+  placeholder: 'Enter your repository URL',
+  validation: {
+    required: {
+      value: true,
+      message: 'repository url is required',
+    },
+    pattern: {
+      value:
+        /^[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i,
+      message: 'not a valid url',
+    },
+  },
+};
+
+export const project_desc_validation = {
+  name: 'desc',
+  label: 'Description',
+  type: 'text',
+  id: 'desc',
+  placeholder: 'Enter your project description',
+  multiline: true,
+  validation: {
+    required: {
+      value: true,
+      message: 'description is required',
+    },
+    minLength: {
+      value: 160,
+      message: 'description should be at least 160 characters',
+    },
+  },
+};
+
+export const project_content_validation = {
+  name: 'md',
+  label: 'Content',
+  type: 'text',
+  id: 'md',
+  placeholder: 'Write some markdown for your project content',
+  multiline: true,
 };
