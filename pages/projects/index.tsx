fa5e80fbc5b701ada3f332f1c99d17f9dbbb0bf7
JakubChorzepa <jakubchorzepa@gmail.com>
2022-10-21 17:01:12 +0200

    Remove hasContainer from projects page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -25,7 +25,7 @@ const Projects = () => {
   useOnClickOutside(ref, handleClickOutside);
 
   return (
-    <SharedLayout title="Projects" hideFooter hasContainer>
+    <SharedLayout title="Projects" hideFooter>
       <div className="flex flex-col">
         <AuthModal title={'Continue with your social accounts'} />
         <div className="sticky top-0 z-10 backdrop-blur-3xl">
