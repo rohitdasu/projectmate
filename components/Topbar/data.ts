4f57850064b70c36e6eff4a7695121c1cd29d7b2
ayush--1 <ayush5april@gmail.com>
2022-10-22 22:28:25 +0530

    added twitter logo in topbar

--- components/Topbar/data.ts
+++ components/Topbar/data.ts
@@ -1,8 +1,8 @@
 import { MdRoofing } from 'react-icons/md';
 import { IoCodeSlash } from 'react-icons/io5';
 import { FiFeather } from 'react-icons/fi';
-import { FaDiscord, FaGithub } from 'react-icons/fa';
-import { TbBrandGithub, TbBrandDiscord } from 'react-icons/tb';
+import { FaDiscord, FaGithub, FaTwitter } from 'react-icons/fa';
+import { TbBrandGithub, TbBrandDiscord, TbBrandTwitter } from 'react-icons/tb';
 import { IconType } from 'react-icons';
 
 type AnchorTagPropsType = {
@@ -49,6 +49,15 @@ export const getSocialLinks = (isSideBar?: boolean): IRoute[] => [
     title: 'GitHub',
     url: 'https://github.com/rohitdasu/projectmate',
     Icon: isSideBar ? TbBrandGithub : FaGithub,
+    anchorTagProps: {
+      target: '_blank',
+      rel: 'noreferrer',
+    },
+  },
+  {
+    title: 'Twitter',
+    url: 'https://twitter.com/projectmateHQ',
+    Icon: isSideBar ? TbBrandTwitter : FaTwitter,
     anchorTagProps: {
       target: '_blank',
       rel: 'noreferrer',
