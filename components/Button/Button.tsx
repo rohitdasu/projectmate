6a6a8d70e2933c1b041ffbeb73ab628ba18a0591
JakubChorzepa <jakubchorzepa@gmail.com>
2022-10-26 20:17:36 +0200

    make button animation optional

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,6 +5,7 @@ export type ButtonProps = {
   type?: 'button' | 'submit';
   onClick: () => void;
   isDisabled: boolean;
+  haveAnimation?: boolean;
   className?: string;
   children: React.ReactNode;
 };
@@ -13,14 +14,15 @@ export const Button: React.FunctionComponent<ButtonProps> = ({
   type = 'button',
   onClick,
   isDisabled,
+  haveAnimation = 'true',
   className,
   children,
 }) => (
   <motion.button
-    whileTap={{ scale: 0.9 }}
+    whileTap={haveAnimation ? { scale: 0.9 } : ''}
     onClick={onClick}
     type={type}
-    className={`items-center rounded-md bg-orange-100 font-semibold text-orange-500 ring-orange-800 focus:ring dark:bg-[#2c1c0f] dark:text-orange-400 ${className} ${
+    className={`items-center rounded-md bg-orange-100 text-orange-500 ring-orange-800 focus:ring dark:bg-[#2c1c0f] dark:text-orange-400 ${className} ${
       isDisabled && 'cursor-default bg-opacity-40 opacity-60'
     }`}
     disabled={isDisabled}
