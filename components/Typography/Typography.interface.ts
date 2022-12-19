cf691a75028ddc84f570ede1abac88622dcde23d
JakubChorzepa <jakubchorzepa@gmail.com>
2022-12-19 09:12:22 +0100

    initiate typography component

---
+++ components/Typography/Typography.interface.ts
@@ -0,0 +1,3 @@
+export type TypographyProps = {
+  component: string;
+};
---
+++ components/Typography/Typography.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { TypographyProps } from './Typography.interface';
+
+export const Typography: React.FunctionComponent<TypographyProps> = () => {
+  return <div>Typography</div>;
+};
