533bf7293089e5a2b1da4364d798f7af08209ba3
JakubChorzepa <jakubchorzepa@gmail.com>
2022-12-26 12:47:46 +0100

    implement typography polimorphic behavior

--- components/Typography/Typography.interface.ts
+++ components/Typography/Typography.interface.ts
@@ -1,3 +1,7 @@
-export type TypographyProps = {
-  component: string;
-};
+import { ReactNode, ElementType, ComponentPropsWithoutRef } from 'react';
+
+export type TypographyProps<C extends ElementType> = {
+  as?: C;
+  children: ReactNode;
+  className: string;
+} & ComponentPropsWithoutRef<C>;
--- components/Typography/Typography.tsx
+++ components/Typography/Typography.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { TypographyProps } from './Typography.interface';
 
-export const Typography: React.FunctionComponent<TypographyProps> = () => {
-  return <div>Typography</div>;
+export const Typography = <C extends React.ElementType = 'span'>({
+  as,
+  children,
+  ...restProps
+}: TypographyProps<C>) => {
+  const Component = as || 'span';
+  return <Component {...restProps}>{children}</Component>;
 };
