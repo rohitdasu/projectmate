396d232368913bd9b5cf0357f45c09dcdee422d9
JakubChorzepa <jakubchorzepa@gmail.com>
2022-12-28 18:07:02 +0100

    add typography text props

--- components/Typography/Typography.interface.ts
+++ components/Typography/Typography.interface.ts
@@ -1,6 +1,6 @@
 import { ReactNode, ElementType, ComponentPropsWithoutRef } from 'react';
 
-export type TextTypeUnion =
+export type TextElementsUnion =
   | 'h1'
   | 'h2'
   | 'h3'
@@ -10,10 +10,41 @@ export type TextTypeUnion =
   | 'span'
   | 'p';
 
-export type TextElementType = Extract<ElementType, TextTypeUnion>;
+export type TextElementType = Extract<ElementType, TextElementsUnion>;
+
+export type Alignment = 'center' | 'inherit' | 'justify' | 'left' | 'right';
+
+export enum FontSizes {
+  'xs' = 'text-xs',
+  'sm' = 'text-sm',
+  'base' = 'text-base',
+  'lg' = 'text-lg',
+  'xl' = 'text-xl',
+  '2xl' = 'text-2xl',
+  '3xl' = 'text-3xl',
+  '4xl' = 'text-4xl',
+  '5xl' = 'text-5xl',
+  '6xl' = 'text-6xl',
+  '7xl' = 'text-7xl',
+}
+
+export type FontSizesUnion = keyof typeof FontSizes;
+
+export enum FontWeights {
+  'light' = 'font-light',
+  'normal' = 'font-normal',
+  'medium' = 'font-medium',
+  'semibold' = 'font-semibold',
+  'bold' = 'font-bold',
+}
+
+export type FontWeightsUnion = keyof typeof FontWeights;
 
 export type TypographyProps<C extends TextElementType> = {
   as?: C;
+  align?: Alignment;
+  fontSize?: FontSizesUnion;
+  fontWeight?: FontWeightsUnion;
   children: ReactNode;
   className: string;
 } & ComponentPropsWithoutRef<C>;
--- components/Typography/Typography.tsx
+++ components/Typography/Typography.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { TypographyProps, TextElementType } from './Typography.interface';
+import {
+  TypographyProps,
+  TextElementType,
+  FontWeights,
+  FontSizes,
+} from './Typography.interface';
 
 export const Typography = <C extends TextElementType = 'span'>({
   as,
+  align = 'left',
+  fontSize = 'base',
+  fontWeight = 'normal',
   children,
+  className,
   ...restProps
 }: TypographyProps<C>) => {
   const Component = as || 'span';
-  return <Component {...restProps}>{children}</Component>;
+  return (
+    <Component
+      className={`${className} ${FontWeights[fontWeight]} ${FontSizes[fontSize]}`}
+      {...restProps}
+    >
+      {children}
+    </Component>
+  );
 };
