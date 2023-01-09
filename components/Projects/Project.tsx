6f6fa525bde0620ef8d18a75906c44f6e0ca806f
Völcsey Zoltán <41576384+zvolcsey@users.noreply.github.com>
2023-01-09 17:37:57 +0100

    feat: Added like feature frontend

--- components/Projects/Project.tsx
+++ components/Projects/Project.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import { ProjectProps } from './Project.interface';
-import { AiOutlineUser } from 'react-icons/ai';
+import { AiOutlineUser, AiFillLike, AiOutlineLike } from 'react-icons/ai';
 import { Tags } from '@/components/Tags';
 import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 import { Button } from '@/components/Button';
+import axios from 'axios';
+import { messageType, toastMessage } from 'shared';
 
 export const Project = ({
   id,
@@ -11,10 +14,51 @@ export const Project = ({
   description,
   tags,
   author,
+  liked,
+  likesCount,
 }: ProjectProps) => {
+  const [likedState, setLikedState] = useState(liked);
+  const [likesState, setLikesState] = useState(likesCount);
   const router = useRouter();
   const handleContributeClick = () => {
     router.push(`/projects/${id}`);
+  };
+
+  const onLike = async () => {
+    try {
+      await axios.post(`/api/project/${id}/like`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      setLikedState(true);
+      setLikesState((prevState) => prevState + 1);
+    } catch (e) {
+      toastMessage(e.message, messageType.error);
+    }
+  };
+
+  const onUnlike = async () => {
+    try {
+      await axios.delete(`/api/project/${id}/like`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      setLikedState(false);
+      setLikesState((prevState) => prevState - 1);
+    } catch (e) {
+      toastMessage(e.message, messageType.error);
+    }
+  };
+
+  const handleLikeClick = async () => {
+    if (!likedState) {
+      await onLike();
+    }
+    if (likedState) {
+      await onUnlike();
+    }
   };
 
   return (
@@ -27,13 +71,28 @@ export const Project = ({
     >
       <div className="flex h-full flex-col items-center overflow-hidden rounded-md text-foreground-1 shadow-border-shadow">
         <div className="flex w-full grow flex-col gap-5 p-4 pt-4">
-          <h2 className="min-w-[0] flex-1 flex-col gap-2 truncate text-xl font-semibold">
-            {title}
-            <span className="flex items-center gap-1 text-sm font-light ">
-              <AiOutlineUser />
-              {author}
-            </span>
-          </h2>
+          <div className="flex w-full items-center">
+            <h2 className="min-w-[0] flex-1 flex-col gap-2 truncate text-xl font-semibold">
+              {title}
+              <span className="flex items-center gap-1 text-sm font-light">
+                <AiOutlineUser />
+                {author}
+              </span>
+            </h2>
+            <div className="flex flex-1 justify-end">
+              <Button
+                onClick={handleLikeClick}
+                isDisabled={false}
+                className="flex items-center px-2 py-1 font-bold sm:my-0"
+              >
+                {likedState ? <AiFillLike /> : <AiOutlineLike />}
+                <span className="px-2">Like</span>
+              </Button>
+              <span className="mx-2 rounded-md bg-background-2 px-2 py-1">
+                {likesState}
+              </span>
+            </div>
+          </div>
           <p className="h-20 text-sm line-clamp-4">{description}</p>
           <div className="flex flex-col gap-5">
             <div className="flex space-x-2 pb-2 md:pb-0">
--- components/Projects/ProjectSkeleton.tsx
+++ components/Projects/ProjectSkeleton.tsx
@@ -9,8 +9,16 @@ export const ProjectSkeleton = () => (
   >
     <div className="flex h-full flex-col justify-center space-y-2 overflow-hidden rounded-md text-foreground-1 shadow-border-shadow">
       <div className="flex flex-col gap-5 p-4 pt-2">
-        <div className="mt-2 h-4 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
-        <div className="h-2 w-36 rounded-full bg-gray-200 dark:bg-gray-700" />
+        <div className="flex w-full items-center">
+          <div className="flex-1 flex-col gap-2">
+            <div className="mt-2 h-4 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+            <div className="mt-2 h-2 w-36 rounded-full bg-gray-200 dark:bg-gray-700" />
+          </div>
+          <div className="flex flex-1 justify-end gap-2">
+            <div className="mt-2 h-7 w-16 rounded-md bg-gray-200 dark:bg-gray-700" />
+            <div className="mt-2 h-7 w-7 rounded-md bg-gray-200 dark:bg-gray-700" />
+          </div>
+        </div>
         <div className="mt-4">
           <div className="h-2 w-full rounded-full bg-gray-200 dark:bg-gray-700" />
           <div className="mt-3 h-2 w-full rounded-full bg-gray-200 dark:bg-gray-700" />
