4112a44f666e6ae311f64fe8588a7da9cbff649d
JakubChorzepa <jakubchorzepa@gmail.com>
2023-02-09 12:11:57 +0100

    add toggling line clamp on description by clicking

--- components/UserBio/UserBio.tsx
+++ components/UserBio/UserBio.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Typography } from '@/components/Typography';
-import { Tag } from '@/components/Tags/Tag';
+import { Tags } from '@/components/Tags';
+import { motion } from 'framer-motion';
 
 export const UserBio = () => {
+  const [isDescriptionClamped, setIsDescriptionClamped] = useState(true);
+
+  const temporarySkills = [
+    'Backend',
+    'Web Development',
+    'Data Structures And Algorithms',
+  ];
+
   return (
-    <div className="flex w-full flex-col justify-between gap-3 p-4">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1 }}
+      layout
+      className="flex w-full flex-col justify-between gap-3 p-4"
+    >
       <div className="flex flex-col">
         <Typography
           as="p"
@@ -36,7 +52,11 @@ export const UserBio = () => {
           as="p"
           fontSize="sm"
           fontWeight="light"
-          className="text-gray-300 line-clamp-4 sm:text-base"
+          onClick={() => setIsDescriptionClamped(!isDescriptionClamped)}
+          className={`cursor-pointer text-gray-300 sm:text-base ${
+            isDescriptionClamped && 'line-clamp-4'
+          }
+          `}
         >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec
           tincidunt magna. Fusce pretium consequat sapien quis blandit. Aliquam
@@ -52,11 +72,11 @@ export const UserBio = () => {
         >
           Skills
         </Typography>
-        <div className="flex flex-row flex-wrap gap-2 py-2">
-          <Tag title="Golang" className="!bg-slate-500" />
-          <Tag title="Typescript" className="!bg-slate-500" />
-          <Tag title="Typescript" className="!bg-slate-500" />
-        </div>
+        <Tags
+          tags={temporarySkills}
+          className="flex-wrap gap-2"
+          tagClassName="bg-slate-500"
+        />
       </div>
       <div className="flex flex-row">
         <Typography
@@ -76,6 +96,6 @@ export const UserBio = () => {
           5
         </Typography>
       </div>
-    </div>
+    </motion.div>
   );
 };
