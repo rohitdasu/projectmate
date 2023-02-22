28942f5d3aa7367312d3d1452d356b764a8f69ef
JakubChorzepa <jakubchorzepa@gmail.com>
2023-02-22 12:23:44 +0100

    change description validation to have less than 80 characters

--- components/EditProfileForm/schema.ts
+++ components/EditProfileForm/schema.ts
@@ -10,7 +10,7 @@ export const schema = z.object({
     .string()
     .min(1, 'Description is required')
     .min(2, 'Description should be at least 2 characters long')
-    .max(20, 'Description should not be greater than 40 characters'),
+    .max(80, 'Description should not be greater than 80 characters'),
   skills: z
     .string()
     .array()
