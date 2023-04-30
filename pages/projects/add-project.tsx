5e55744db5cce20cd358f3a064d2b430a80d0d8d
Yazdun <83041367+Yazdun@users.noreply.github.com>
2023-04-30 20:04:43 +0330

    feat: updated "add project" form (#379)
    * chore: updated add project form layout
    
    * fixed form layout issue
    
    * fix form layout
    
    * fix add project form layout

--- pages/projects/add-project.tsx
+++ pages/projects/add-project.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Header, AddProjectForm } from '@/components';
 import Head from 'next/head';
+import Image from 'next/image';
+import IMAGE from '@/assets/ssscribble.svg';
 
 const AddProject = () => {
   return (
@@ -10,9 +12,18 @@ const AddProject = () => {
         <link rel="icon" href="/logo.svg" />
       </Head>
       <Header homeRoute="/projects" />
-      <div className="mx-auto mt-4 flex h-[calc(100vh-80px)] w-full max-w-screen-sm flex-col items-start justify-start gap-2 md:gap-4">
-        <h2 className="px-4 text-xl text-gray-300 md:text-2xl">Add project</h2>
-        <AddProjectForm />
+      <div className="mx-auto  min-h-[calc(100vh-80px)] w-full max-w-screen-xl pb-0">
+        <div className=" overflow-hidden  rounded-xl md:bg-[rgba(0,0,0,0.5)] xl:flex xl:items-center">
+          <div className="flex w-full flex-col gap-4 md:p-10 xl:w-[50%]">
+            <h2 className="px-4 text-xl text-gray-300 md:text-2xl">
+              Add project
+            </h2>
+            <AddProjectForm />
+          </div>
+          <div className="relative hidden xl:block xl:w-[50%]">
+            <Image src={IMAGE} alt="" />
+          </div>
+        </div>
       </div>
     </>
   );
