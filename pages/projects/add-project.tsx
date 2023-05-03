c5721a914b440a990e758da01d79c06315f792f6
Rohit <48400770+rohitdasu@users.noreply.github.com>
2023-05-03 13:19:38 +0530

    chore: removed gradient background (#381)
    * chore: removed gradient background
    
    * fix: removed height calcs

--- pages/projects/add-project.tsx
+++ pages/projects/add-project.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Header, AddProjectForm } from '@/components';
 import Head from 'next/head';
 import Image from 'next/image';
-import IMAGE from '@/assets/ssscribble.svg';
+import IMAGE from '@/assets/scribble.svg';
 
 const AddProject = () => {
   return (
@@ -11,17 +11,19 @@ const AddProject = () => {
         <title>Add Project | Projectmate</title>
         <link rel="icon" href="/logo.svg" />
       </Head>
-      <Header homeRoute="/projects" />
-      <div className="mx-auto  min-h-[calc(100vh-80px)] w-full max-w-screen-xl pb-0">
-        <div className=" overflow-hidden  rounded-xl md:bg-[rgba(0,0,0,0.5)] xl:flex xl:items-center">
-          <div className="flex w-full flex-col gap-4 md:p-10 xl:w-[50%]">
-            <h2 className="px-4 text-xl text-gray-300 md:text-2xl">
-              Add project
-            </h2>
-            <AddProjectForm />
-          </div>
-          <div className="relative hidden xl:block xl:w-[50%]">
-            <Image src={IMAGE} alt="" />
+      <div className="flex h-screen flex-col">
+        <Header homeRoute="/projects" />
+        <div className="mx-auto w-full max-w-screen-xl flex-1 pb-0 md:mb-4">
+          <div className="rounded-xl xl:flex xl:items-center xl:bg-[rgba(0,0,0,0.5)]">
+            <div className="flex w-full flex-col gap-4 md:p-10 xl:w-[50%]">
+              <h2 className="px-4 text-xl text-gray-300 md:text-2xl">
+                Add project
+              </h2>
+              <AddProjectForm />
+            </div>
+            <div className="relative hidden xl:block xl:w-[50%]">
+              <Image src={IMAGE} alt="scribble" />
+            </div>
           </div>
         </div>
       </div>
