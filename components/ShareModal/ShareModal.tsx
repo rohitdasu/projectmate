bd2e97dd4e1fb5da4b4959fea941e14c63f897a8
JakubChorzepa <jakubchorzepa@gmail.com>
2023-07-01 16:56:20 +0200

    add ui to display and copy project url

--- components/ShareModal/ShareModal.tsx
+++ components/ShareModal/ShareModal.tsx
@@ -50,16 +50,21 @@ export const ShareModal: React.FC<ShareModalProps> = ({
                   leaveFrom="opacity-100 scale-100"
                   leaveTo="opacity-0 scale-95"
                 >
-                  <Dialog.Panel className="flex w-full max-w-md transform flex-col justify-center gap-4 overflow-hidden rounded-2xl border-[0.1px] bg-slate-900 px-6 pt-6 pb-12 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Panel className="flex w-full max-w-md transform flex-col justify-center gap-7  overflow-hidden rounded-2xl border-[0.1px] bg-slate-900 px-6 pt-6 pb-12 text-left align-middle shadow-xl transition-all">
                     <div className="  flex flex-row place-content-between">
-                      <Typography as="h1" fontSize="lg" align="left">
+                      <Typography
+                        as="h1"
+                        fontSize="lg"
+                        align="left"
+                        fontWeight="semibold"
+                      >
                         Share project
                       </Typography>
                       <button className="cursor-pointer" onClick={closeModal}>
                         <IoClose size={30} />
                       </button>
                     </div>
-                    <div className="flex flex-row gap-4">
+                    <div className="flex flex-row flex-wrap gap-4">
                       <EmailShareButton subject={projectTitle} url={projectUrl}>
                         <EmailIcon size={40} round />
                       </EmailShareButton>
@@ -79,6 +84,20 @@ export const ShareModal: React.FC<ShareModalProps> = ({
                       >
                         <LinkedinIcon size={40} round />
                       </LinkedinShareButton>
+                    </div>
+                    <div className="flex flex-row gap-3">
+                      <div className="w-1/2 rounded-lg bg-gray-700 py-2 px-4">
+                        <Typography
+                          as="span"
+                          className="block truncate"
+                          fontSize="sm"
+                        >
+                          {projectUrl}
+                        </Typography>
+                      </div>
+                      <button className="rounded-lg bg-primary-color px-3">
+                        Copy
+                      </button>
                     </div>
                   </Dialog.Panel>
                 </Transition.Child>
