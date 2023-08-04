57112178854bfcd7995f3ce4fd9208c4eb656fb5
JakubChorzepa <jakubchorzepa@gmail.com>
2023-08-04 16:14:19 +0200

    chore: add combineProviders util

---
+++ lib/combineProviders.tsx
@@ -0,0 +1,11 @@
+import React, { FC, PropsWithChildren } from 'react';
+
+export const combineProviders = (providers: FC<PropsWithChildren>[]) => {
+  const CombinedProvider = ({ children }: PropsWithChildren) => {
+    return providers.reduce((acc, Provider) => {
+      return <Provider>{acc}</Provider>;
+    }, children);
+  };
+
+  return CombinedProvider;
+};
