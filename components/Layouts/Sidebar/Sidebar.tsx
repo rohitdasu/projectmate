23e9cc2e0901aa2b373d917a0e7020c9f8edd9b9
Aryan <aryan.khubchandani@gmail.com>
2023-08-05 20:10:11 +0530

    feat: floating action button

--- components/Layouts/Sidebar/Sidebar.tsx
+++ components/Layouts/Sidebar/Sidebar.tsx
@@ -86,8 +86,8 @@ export const Sidebar = () => {
           onClick={handleAddProject}
           className="flex h-9 cursor-pointer items-center text-gray-500 transition-all hover:text-gray-200"
         >
-          <div className="block rounded-full bg-green-600 p-0.5 lg:hidden">
-            <MdAdd size={20} color={'white'} />
+          <div className="fixed bottom-5 right-5 block rounded-full bg-green-600 p-2 lg:hidden">
+            <MdAdd size={45} color={'white'} />
           </div>
           <button
             className={`hidden rounded-2xl bg-green-600 py-2 px-4 text-base text-gray-200 hover:text-white lg:block`}
