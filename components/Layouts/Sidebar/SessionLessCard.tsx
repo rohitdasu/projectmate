e8a30ff2914cd8131b9d29f74ace71e0b9274f56
Aryan Khubchandani <76617460+AryanKhubchandani@users.noreply.github.com>
2023-08-07 21:38:49 +0530

    feat: navbar revamp (#412)
    * feat: mates section
    
    * fix: remove heading
    
    * fix: interface naming convention
    
    * fix: url query
    
    * feat: add loading state
    
    * refactor: src
    
    * feat: navbar
    
    * fix: remove condition and change icon
    
    * fix: reduce icon size
    
    * feat: update add project icon
    
    * feat: reduce icons size
    
    * feat: floating action button
    
    * fix: button padding
    
    * fix: avatar logo alignment
    
    * feat: show initials for no profile picture
    
    * fix: z-index
    
    * refactor: remove state
    
    * fix: remove block

--- components/Layouts/Sidebar/SessionLessCard.tsx
+++ components/Layouts/Sidebar/SessionLessCard.tsx
@@ -3,7 +3,7 @@ import { AuthModal } from '@/components/AuthModal';
 import { useAppDispatch } from '@/hooks';
 import { openModal } from '@/store/slices/sliceModal';
 import { IoLogIn } from 'react-icons/io5';
-import { MdPlaylistAdd } from 'react-icons/md';
+import { MdLogin } from 'react-icons/md';
 
 export const SessionLessCard = () => {
   const dispatch = useAppDispatch();
@@ -14,25 +14,14 @@ export const SessionLessCard = () => {
       <AuthModal title={loginMessage} />
       <li
         onClick={() => {
-          setLoginMessage('Login with your account to add project');
-          dispatch(openModal());
-        }}
-        className="mt-2 h-9 cursor-pointer text-gray-500 transition-all hover:text-gray-200"
-      >
-        <MdPlaylistAdd size={28} className="block lg:hidden" />
-        <span className="hidden text-lg text-gray-400 hover:text-gray-300 lg:block ">
-          Add project
-        </span>
-      </li>
-      <li
-        onClick={() => {
           setLoginMessage(message);
           dispatch(openModal());
         }}
-        className="h-9 cursor-pointer text-gray-500 transition-all hover:text-gray-200"
+        className="h-9 cursor-pointer text-gray-400 transition-all hover:text-gray-200"
       >
         <IoLogIn size={27} className="block lg:hidden" />
-        <span className="hidden text-lg text-green-400 transition-colors hover:text-green-500 lg:block">
+        <span className="hidden items-center gap-4 text-lg lg:flex">
+          <MdLogin size={25} />
           Login
         </span>
       </li>
--- components/Layouts/Sidebar/Sidebar.tsx
+++ components/Layouts/Sidebar/Sidebar.tsx
@@ -5,6 +5,11 @@ import { useRouter } from 'next/router';
 import { SessionCard } from './SessionCard';
 import { SessionLessCard } from './SessionLessCard';
 import { NavRoutes } from './data';
+import { MdAdd } from 'react-icons/md';
+import { useAppDispatch } from '@/hooks';
+import { useState } from 'react';
+import { openModal } from '@/store/slices/sliceModal';
+import { AuthModal } from '@/components/AuthModal';
 
 const NavElements = NavRoutes.map((nav) => {
   return {
@@ -34,13 +39,26 @@ const Logo = () => {
 
 export const Sidebar = () => {
   const { data: session, status } = useSession();
-  const { pathname } = useRouter();
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const message = 'Continue with your social account';
+  const [loginMessage, setLoginMessage] = useState(message);
+
+  const handleAddProject = () => {
+    if (status === 'authenticated') {
+      router.push('/projects/add-project');
+    } else {
+      setLoginMessage('Login with your account to add project');
+      dispatch(openModal());
+    }
+  };
+
   return (
-    <div className="fixed inset-0 flex h-screen w-[11%] flex-col items-center px-2 pt-6 md:items-start md:px-8 lg:w-1/4">
+    <div className="fixed inset-0 z-10 flex h-screen w-[11%] flex-col items-center px-2 pt-6 md:items-start md:px-8 lg:w-1/4">
       <Logo />
       <ul className="mt-16 flex w-full flex-col items-center justify-center gap-4 md:items-start">
         {NavElements.map((nav) => {
-          const isActive = pathname === nav.link;
+          const isActive = router.pathname === nav.link;
           return (
             <Link key={nav.id} href={nav.link}>
               <li
@@ -49,20 +67,34 @@ export const Sidebar = () => {
                 }`}
               >
                 {nav.icon}
-                <span className="hidden text-lg text-gray-400 hover:text-gray-300 lg:block ">
+                <span className="hidden text-lg text-gray-400 hover:text-gray-300 lg:block">
                   {nav.name}
                 </span>
               </li>
             </Link>
           );
         })}
+        <AuthModal title={loginMessage} />
+        <li
+          onClick={handleAddProject}
+          className="flex h-9 cursor-pointer items-center text-gray-500 transition-all hover:text-gray-200"
+        >
+          <div className="fixed bottom-5 right-5 block cursor-pointer rounded-full bg-green-600 p-2	lg:hidden">
+            <MdAdd size={45} color={'white'} />
+          </div>
+          <button
+            className={`hidden rounded-2xl bg-green-600 py-2 px-8 text-base text-gray-200 hover:text-white lg:block`}
+          >
+            Add project
+          </button>
+        </li>
       </ul>
-      <ul className="mt-4 flex w-full flex-col items-center gap-4 border-t border-gray-800 pt-4 transition-all md:items-start">
+      <ul className="absolute bottom-2 mt-4 flex w-3/4 flex-col items-center gap-4 border-t border-gray-800 pt-4 transition-all md:items-start">
         {status === 'authenticated' ? (
           <SessionCard
-            email={session.user?.email || ''}
-            name={session.user?.name || ''}
-            image={session.user?.image || ''}
+            email={session?.user?.email || ''}
+            name={session?.user?.name || ''}
+            image={session?.user?.image || ''}
           />
         ) : status === 'unauthenticated' ? (
           <SessionLessCard />
