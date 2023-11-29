002f78fe1adfbe0781b0224e1aa7ea1b7df3061c
rohitdasu <dasurohit123@gmail.com>
2023-11-29 22:36:38 +0530

    feat: added profile load skeleton

--- components/views/ProfilePage/ProfilePage.tsx
+++ components/views/ProfilePage/ProfilePage.tsx
@@ -1,9 +1,9 @@
+import React, { useEffect, useState } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
-import React, { useEffect, useState } from 'react';
 import { ProfilePageProps } from './ProfilePage.interface';
-import { CrownIcon, Loader, Verified } from 'lucide-react';
+import { CrownIcon, Verified } from 'lucide-react';
 import { ProfilePageProject } from './ProfilePageProject';
 import {
   Sheet,
@@ -17,6 +17,7 @@ import {
 } from '@/components/ui/sheet';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
+import { ProfileProjectSkeleton } from './ProfileProjectSkeleton';
 
 export const ProfilePage = (profile: ProfilePageProps) => {
   const getFallbackName = () => {
@@ -36,32 +37,25 @@ export const ProfilePage = (profile: ProfilePageProps) => {
       window.addEventListener('resize', handleResize);
       return () => window.removeEventListener('resize', handleResize);
     }
+    return undefined;
   }, []);
 
   // Determine the side based on window width
   const sheetSide = windowWidth && windowWidth < 768 ? 'bottom' : 'right';
 
-  if (
-    profile.isDetailsLoading ||
-    profile.isProjectsLoading ||
-    profile.isGoogleLoading
-  ) {
-    return (
-      <div className="flex h-full items-center justify-center">
-        <Loader className="h-10 w-10 animate-spin" />
-      </div>
-    );
-  }
-
   return (
     <div className="w-full py-4 px-4 md:px-0 md:py-10">
       <section className="flex flex-row items-center justify-between">
-        <Avatar className="h-16 w-16 rounded-lg md:h-24 md:w-24">
-          <AvatarImage src={profile.profile?.user?.image || undefined} />
-          <AvatarFallback className="rounded-lg text-xl md:text-4xl">
-            {getFallbackName()}
-          </AvatarFallback>
-        </Avatar>
+        {!profile.isGoogleLoading ? (
+          <Avatar className="h-16 w-16 rounded-lg md:h-24 md:w-24">
+            <AvatarImage src={profile.profile?.user?.image || undefined} />
+            <AvatarFallback className="rounded-lg text-xl md:text-4xl">
+              {getFallbackName()}
+            </AvatarFallback>
+          </Avatar>
+        ) : (
+          <div className="h-16 w-16 animate-pulse rounded-lg bg-gray-700 md:h-24 md:w-24" />
+        )}
         <Sheet>
           <SheetTrigger asChild>
             <Button variant={'outline'} disabled>
@@ -98,60 +92,101 @@ export const ProfilePage = (profile: ProfilePageProps) => {
         </Sheet>
       </section>
       <section className="my-2">
-        <p className="flex items-center gap-2 text-base font-semibold md:text-xl">
-          {profile.profile?.user?.name}
-          {profile?.projects?.results?.length &&
-          profile.projects.results.length < 0 ? (
-            <Badge className="bg-yellow-400 text-yellow-900 hover:bg-yellow-400/80">
-              <CrownIcon className="h-4" /> Gold Member
-            </Badge>
-          ) : (
-            <Badge className="bg-blue-500 text-white hover:bg-blue-500/80">
-              <Verified className="h-4" /> Verified Member
-            </Badge>
-          )}
-        </p>
-        {profile.details?.results.title ? (
-          <p className="text-base text-black dark:text-white md:text-lg">
-            {profile.details.results.title}
-          </p>
+        {!profile.isProjectsLoading && !profile.isGoogleLoading ? (
+          <div className="flex items-center gap-2 text-base font-semibold md:text-xl">
+            <p>{profile.profile?.user?.name}</p>
+            {profile?.projects?.results?.length &&
+            profile.projects.results.length < 0 ? (
+              <Badge className="bg-yellow-400 text-yellow-900 hover:bg-yellow-400/80">
+                <CrownIcon className="h-4" /> Gold Member
+              </Badge>
+            ) : (
+              <Badge className="bg-blue-500 text-white hover:bg-blue-500/80">
+                <Verified className="h-4" /> Verified Member
+              </Badge>
+            )}
+          </div>
         ) : (
-          <p className="text-base text-black opacity-80 dark:text-white md:text-lg">
-            Title, ex: Software Engineer | Frontend Developer
-          </p>
+          <section className="flex animate-pulse items-center gap-2">
+            <p className="h-5 w-28 bg-gray-700" />
+            <Badge className="h-5 w-36 bg-gray-700"></Badge>
+          </section>
         )}
-        {profile.details?.results.description ? (
-          <p className="text-sm text-gray-900 dark:text-gray-100 md:text-base">
-            {profile.details.results.description}
-          </p>
+        {profile.isDetailsLoading ? (
+          <p className="mt-2 h-4 w-40 animate-pulse bg-gray-700" />
         ) : (
-          <p className="text-sm text-gray-900 opacity-80 dark:text-gray-100 md:text-base">
-            Description, ex: Hey there, I am Rohit, a developer from India.
-          </p>
+          <>
+            {profile.details?.results.title ? (
+              <p className="text-base text-black dark:text-white md:text-lg">
+                {profile.details.results.title}
+              </p>
+            ) : (
+              <p className="text-base text-black opacity-80 dark:text-white md:text-lg">
+                Title, ex: Software Engineer | Frontend Developer
+              </p>
+            )}
+          </>
+        )}
+
+        {profile.isDetailsLoading ? (
+          <p className="mt-2 h-4 w-64 animate-pulse bg-gray-700 md:w-80" />
+        ) : (
+          <>
+            {profile.details?.results.description ? (
+              <p className="text-sm text-gray-900 dark:text-gray-100 md:text-base">
+                {profile.details.results.description}
+              </p>
+            ) : (
+              <p className="text-sm text-gray-900 opacity-80 dark:text-gray-100 md:text-base">
+                Description, ex: Hey there, I am Rohit, a developer from India.
+              </p>
+            )}
+          </>
         )}
       </section>
       <div className="flex flex-row flex-wrap gap-2">
-        {profile.details?.results.skills ? (
-          profile.details.results.skills.map((skill, idx) => (
-            <Badge className="text-sm" variant={'secondary'} key={idx}>
-              {skill}
-            </Badge>
-          ))
+        {profile.isDetailsLoading ? (
+          <>
+            <Badge className="h-5 w-20 animate-pulse bg-gray-700"></Badge>
+            <Badge className="h-5 w-16 animate-pulse bg-gray-700"></Badge>
+            <Badge className="h-5 w-24 animate-pulse bg-gray-700"></Badge>
+          </>
         ) : (
-          <p className="text-lg opacity-80">Skills/Interests show here</p>
+          <>
+            {profile.details?.results.skills ? (
+              profile.details.results.skills.map((skill, idx) => (
+                <Badge className="text-sm" variant={'secondary'} key={idx}>
+                  {skill}
+                </Badge>
+              ))
+            ) : (
+              <p className="text-lg opacity-80">Skills/Interests show here</p>
+            )}
+          </>
         )}
       </div>
       <div className="my-6 grid grid-cols-1 gap-2 lg:grid-cols-2">
-        {profile?.projects?.results?.length ? (
-          profile.projects.results.map((project, idx) => (
-            <ProfilePageProject
-              title={project.title}
-              description={project.description}
-              key={idx}
-            />
-          ))
-        ) : (
-          <p className="text-lg opacity-80">No projects 💔</p>
+        {profile.isProjectsLoading && (
+          <>
+            {Array.from({ length: 9 }).map((_, index) => (
+              <ProfileProjectSkeleton key={index} />
+            ))}
+          </>
+        )}
+        {!profile.isProjectsLoading && (
+          <>
+            {profile?.projects?.results?.length ? (
+              profile.projects.results.map((project, idx) => (
+                <ProfilePageProject
+                  title={project.title}
+                  description={project.description}
+                  key={idx}
+                />
+              ))
+            ) : (
+              <p className="text-lg opacity-80">No projects 💔</p>
+            )}
+          </>
         )}
       </div>
     </div>
---
+++ components/views/ProfilePage/ProfileProjectSkeleton.tsx
@@ -0,0 +1,23 @@
+export const ProfileProjectSkeleton = () => {
+  return (
+    <div className="animate-pulse rounded-lg border bg-card p-4 text-card-foreground shadow-sm">
+      <div className="flex flex-col gap-2">
+        <div className="h-6 w-36 bg-gray-700" />
+        <div className="flex flex-col gap-1">
+          <p className="h-3 w-full bg-gray-700" />
+          <p className="h-3 w-full bg-gray-700" />
+          <p className="h-3 w-full bg-gray-700" />
+          <p className="h-3 w-full bg-gray-700" />
+          <p className="h-3 w-full bg-gray-700" />
+          <p className="h-3 w-full bg-gray-700" />
+          <p className="h-3 w-full bg-gray-700" />
+          <p className="h-3 w-3/4 bg-gray-700" />
+        </div>
+      </div>
+      <div className="mt-4 flex gap-2">
+        <div className="h-10 w-20 rounded-md bg-gray-700"></div>
+        <div className="h-10 w-16 rounded-md bg-gray-700"></div>
+      </div>
+    </div>
+  );
+};
