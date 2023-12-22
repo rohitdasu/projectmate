af7e512d7294eab071368ceefc65abfac0be923c
Yuba Neupane <neopaneyb2@gmail.com>
2023-12-22 12:02:51 -0500

    added FAB to go to the top of the page

---
+++ components/ui/back-to-top-button.tsx
@@ -0,0 +1,53 @@
+'use client';
+import { useState, useEffect } from 'react';
+import { animateScroll as scroll } from 'react-scroll';
+import { FaArrowUp } from 'react-icons/fa';
+import { Button } from './button';
+
+const BackToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    const yOffset = window.scrollY;
+
+    if (yOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const handleScrollToTop = () => {
+    scroll.scrollToTop({ duration: 500, smooth: 'easeInOutQuad' });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        bottom: '20px',
+        right: '20px',
+        opacity: isVisible ? 1 : 0,
+        transition: 'opacity 0.3s',
+      }}
+    >
+      <Button
+        variant="default"
+        onClick={handleScrollToTop}
+        className="h-[60px] w-[60px] rounded-full drop-shadow-lg"
+      >
+        <FaArrowUp size={48} />
+      </Button>
+    </div>
+  );
+};
+
+export default BackToTopButton;
