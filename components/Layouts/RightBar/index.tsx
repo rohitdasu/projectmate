b9bdd3345fc0ef3d41d8f051a3fa0bd4de9ee141
shovonroy173 <shovonroy2003@gmail.com>
2024-03-24 22:13:20 +0530

    feat: shadcnui used

--- components/Layouts/RightBar/index.tsx
+++ components/Layouts/RightBar/index.tsx
@@ -2,26 +2,33 @@ import { Button } from '@/components/ui/button';
 import Header from '@/components/ui/header';
 import useGetAllTags from '@/hooks/useGetAllTags';
 import { useRouter } from 'next/router';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 
 const RightBar = () => {
   const { data: tags } = useGetAllTags();
+  // console.log("LINE AT 9" , tags);
   // const allUniqueTag = tags?.results;
   const router = useRouter();
   const { tag } = router.query;
-  console.log(tag);
-
+  // console.log(tag);
+  const [flag, setFlag] = useState(true);
+  // console.log("LINE AT 16" , flag);
   const handleClick = useCallback(
     (e: any, tag: string) => {
       e.stopPropagation();
-
-      const url = `/tprojects/${tag}`;
-      router.push(url);
+      if (flag) {
+        const url = `/tprojects/${tag}`;
+        router.push(url);
+        setFlag(!flag);
+      } else {
+        router.push('/projects');
+        setFlag(!flag);
+      }
     },
-    [router]
+    [router, flag]
   );
   return (
-    <div className="fixed  z-10 h-dvh  items-center px-2 pt-6 md:items-start md:px-8 lg:w-1/4">
+    <div className="fixed   h-dvh  items-center px-2 pt-6 md:items-start md:px-8 lg:w-1/4">
       <Header data="Tags" />
       {tags?.results.map((item: string, index: number) => {
         return (
@@ -31,7 +38,7 @@ const RightBar = () => {
             variant={item === tag ? 'secondary' : 'default'}
             onClick={(e) => handleClick(e, item)}
           >
-            {item}
+            {item.toLowerCase()}
           </Button>
         );
       })}
