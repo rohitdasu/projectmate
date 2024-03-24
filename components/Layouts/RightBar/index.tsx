eb1e65186a1d9f067c8eb214cc0214dc512f59cf
shovonroy173 <shovonroy2003@gmail.com>
2024-03-24 13:13:00 +0530

    feat: TAGS added at the rightbar

---
+++ components/Layouts/RightBar/index.tsx
@@ -0,0 +1,37 @@
+import { Button } from '@/components/ui/button';
+import useGetAllTags from '@/hooks/useGetAllTags';
+import { useRouter } from 'next/router';
+import React, { useCallback } from 'react';
+
+const RightBar = () => {
+  const { data: tags } = useGetAllTags();
+  // const allUniqueTag = tags?.results;
+  const router = useRouter();
+
+  const handleClick = useCallback(
+    (e: any, tag: string) => {
+      e.stopPropagation();
+
+      const url = `/tprojects/${tag}`;
+      router.push(url);
+    },
+    [router]
+  );
+  return (
+    <div className="mt-7">
+      {tags?.results.map((item: string, index: number) => {
+        return (
+          <Button
+            key={index}
+            className="mb-2 mr-2"
+            onClick={(e) => handleClick(e, item)}
+          >
+            {item}
+          </Button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default RightBar;
