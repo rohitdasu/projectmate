d1225c9948887d32ba28c35a5825eaf5f7990459
shovonroy173 <shovonroy2003@gmail.com>
2024-03-25 23:26:18 +0530

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -59,17 +59,22 @@ module.exports = {
       },
       keyframes: {
         'accordion-down': {
-          from: { height: '0' },
+          from: { height: 0 },
           to: { height: 'var(--radix-accordion-content-height)' },
         },
         'accordion-up': {
           from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
+          to: { height: 0 },
+        },
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(5deg)' },
+          '50%': { transform: 'rotate(-5deg)' },
         },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        wiggle: 'wiggle 1s ease-in-out infinite',
       },
     },
   },
